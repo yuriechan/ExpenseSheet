8a773136010c1108d1627d09f2ba95549d8a96e2
Yurie <cutecupcakeandcute@gmail.com>
2018-10-03 16:53:19 +0900

    add "delete btn"&"add btn"

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var $TABLE = $('.table');
+$(document).ready(function(){
 
-$('#add-table').click(function (){
-  var $clone = $TABLE.find('tr.hide').clone();
-  //$TABLE.find('table').appendTo('$clone');
-})
+  $('#add-table').click(function (){
+    $('table').append('<tr><td><span id = "add-table" class="glyphicon glyphicon-plus"></span><input /></td><td><input /></td><td><input /></td><td><input /><button id = "delete-table">DELETE</button></td></tr>')
+  })
+
+  $('#delete-table').click(function (){
+    $(this).parents('tr').remove();
+  })
+
+
+
+  });
