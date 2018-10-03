c34d3d90483f19fbc58d7632c4cfa66b1130288e
Yurie <cutecupcakeandcute@gmail.com>
2018-10-03 19:06:10 +0900

    add new row to the end of column when add btn clicked

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
-  $('#add-table').click(function (){
-    $('table').append('<tr><td><span id = "add-table" class="glyphicon glyphicon-plus"></span><input /></td><td><input /></td><td><input /></td><td><input /><button id = "delete-table">DELETE</button></td></tr>')
+  $(document).on('click', '#add-table', function (){
+    $('table').after('<tr><td><span id = "add-table" class="glyphicon glyphicon-plus"></span><input id = "pickdate"/></td><td><input /></td><td><input /></td><td><input /><button id = "delete-table">DELETE</button></td></tr>');
   })
 
   $('#delete-table').click(function (){
     $(this).parents('tr').remove();
   })
 
+  $('#pickdate').datepicker();
 
 
   });
