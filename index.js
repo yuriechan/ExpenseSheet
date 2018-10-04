41be9862bdb493fc08b227a847119c27a3d42a9e
Yurie <cutecupcakeandcute@gmail.com>
2018-10-04 11:59:34 +0900

    add row one after the clicked element & add delete btn for each row

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 $(document).ready(function(){
+var counter = 0;
+  // $(document).on('click', '#add-table', function (){
+  //   $('table').after('<tr><td><span id = "add-table" class="glyphicon glyphicon-plus"></span><input id = "pickdate"/></td><td><input /></td><td><input /></td><td><input /><button id = "delete-table">DELETE</button></td></tr>');
+  // })
+  //
+  // $('#delete-table').click(function (){
+  //   $(this).parents('tr').remove();
+  // })
+  //
+  // $('#pickdate').datepicker();
 
-  $(document).on('click', '#add-table', function (){
-    $('table').after('<tr><td><span id = "add-table" class="glyphicon glyphicon-plus"></span><input id = "pickdate"/></td><td><input /></td><td><input /></td><td><input /><button id = "delete-table">DELETE</button></td></tr>');
-  })
+  $(document).on('click','#add-table', function(){
+    var counter = $('table tr').length - 1;
+    alert(counter)
 
-  $('#delete-table').click(function (){
-    $(this).parents('tr').remove();
-  })
+    $(document).on('click','#delete-table', function(){
+      counter = counter - 1;
+      console.log(counter);
+    })
 
-  $('#pickdate').datepicker();
+
+  var newRow = $('<tr>');
+  var cols = '';
+
+  cols += '<td><span id = "add-table" class="glyphicon glyphicon-plus"></span><input id = "pickdate"/></td>'
+  cols += '<td><input name = "name'+ counter +'"></td>';
+  cols += '<td><input name = "type '+ counter +'"></td>';
+  cols += '<td><input name = "cost '+ counter +'"></td>';
+  cols += '<td><button type = "button" id = "delete-table">DELETE</button></td>'
+
+  newRow.append(cols);
+
+  newRow.insertAfter($(this).parents().closest('tr'));
+
+  counter = counter + 1;
+    })
+
+    $(document).on('click','#delete-table', function(){
+      $(this).closest('tr').remove();
+    })
 
 
   });
