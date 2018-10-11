6773fb4229f19da824ccaf17a51dafe080e87ea3
Yurie <cutecupcakeandcute@gmail.com>
2018-10-11 18:31:50 +0900

    added oAuth to access the user's google spreadsheet to read data

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-for ($row = 0; $row < 8; $row++){
-  echo "<div style = 'clear:both'>";
-    for ($col = 0; $col < 3; $col++){
-      echo "<input type = 'text' style = 'float:left;' name = '$row:$col' id = '$row:$col'>";
-    }
-  echo "</div>";
-}
-?>
