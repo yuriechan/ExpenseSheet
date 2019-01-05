5b06c8f1514f95277a3a7a6dfa85cf2caf79bd07
Yurie <cutecupcakeandcute@gmail.com>
2019-01-05 16:52:48 +0900

    BUG: user input not displayed in db

--- dbcode.sql
+++ dbcode.sql
@@ -1,6 +1,9 @@
 CREATE TABLE expenses (
+  id int(11) AUTO_INCREMENT PRIMARY KEY not null,
   date int(11),
   category text not null,
   description text not null,
   price int(11)
 )
+
+INSERT INTO expenses (date, category, description, price) VALUES ('$date', '$category', '$description', '$price');
